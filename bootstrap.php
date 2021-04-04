51a3ac91b2eb6793342bbd18d653acb40b2d7f91
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 18:36:57 -0400

    Add the service function using psr standards.

--- bootstrap.php
+++ bootstrap.php
@@ -4,3 +4,11 @@
 if (function_exists('service')) {
     throw new Omatamix\Container\Exception\ContainerException('The `service` function already exists.');
 }
+
+// Add a service helper.
+// MAY return mixed.
+function service(string $id)
+{
+    $instance = Omatamix\Container\StaticContainer::getInstance();
+    return $instance->get($id);
+}

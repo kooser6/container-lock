ddecf09d33a4691d52bc0758b8babb5400177cad
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 19:10:36 -0400

    Fix errors.

--- src/Exception/ContainerException.php
+++ src/Exception/ContainerException.php
@@ -30,6 +30,6 @@ use Psr\Container\ContainerExceptionInterface;
 /**
  * Exceptions directly thrown by the container.
  */ 
-class ContainerException extends ExceptionInterface implements ContainerExceptionInterface
+class ContainerException implements ExceptionInterface, ContainerExceptionInterface
 {
 }

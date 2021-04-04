d72ef048f682101c41b4194eafedfea5b5af42e4
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 19:09:01 -0400

    Fix errors.

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -28,6 +28,6 @@ namespace Omatamix\Container\Exception;
 /**
  * The interface for all exceptions.
  */ 
-class ExceptionInterface extends \Throwable
+interface ExceptionInterface extends \Throwable
 {
 }

8c8501262adc511fa44c100839779e39e6f03127
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 19:58:57 -0400

    Update ContainerException.php

--- src/Exception/ContainerException.php
+++ src/Exception/ContainerException.php
@@ -30,6 +30,6 @@ use Psr\Container\ContainerExceptionInterface;
 /**
  * Exceptions directly thrown by the container.
  */ 
-class ContainerException extends \Throwable implements ExceptionInterface, ContainerExceptionInterface
+class ContainerException extends \Exception implements ExceptionInterface, ContainerExceptionInterface
 {
 }

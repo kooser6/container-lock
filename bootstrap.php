38d5c81ce774e7b4556c67c1589cf033d6a912c5
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 20:03:07 -0400

    Fix errors.

--- bootstrap.php
+++ bootstrap.php
@@ -1,24 +1,23 @@
 <?php
 
-// Ensure the service function does not exist.
-if (function_exists('service_get') || function_exists('service_has')) {
-    throw new Omatamix\Container\Exception\ContainerException('The `service_get` or `service_has` function already exists.');
-}
-
 // Add a service helper `get`.
 // @throws \Omatamix\Container\Exception\ContainerException If the instance is null.
 // MAY return mixed.
-function service_get(string $id)
-{
-    $instance = Omatamix\Container\StaticContainer::getInstance();
-    return $instance->get($id);
+if (function_exists('service_get')) {
+    function service_get(string $id)
+    {
+        $instance = Omatamix\Container\StaticContainer::getInstance();
+        return $instance->get($id);
+    }
 }
 
 // Add a service helper `get`.
 // @throws \Omatamix\Container\Exception\ContainerException If the instance is null.
 // MUST return bool.
-function service_has(string $id)
-{
-    $instance = Omatamix\Container\StaticContainer::getInstance();
-    return $instance->has($id);
+if (function_exists('service_has')) {
+    function service_has(string $id)
+    {
+        $instance = Omatamix\Container\StaticContainer::getInstance();
+        return $instance->has($id);
+    }
 }

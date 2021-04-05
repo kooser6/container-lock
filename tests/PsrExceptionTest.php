e0487f62a056f006a9fbadf43336bfc3bb336407
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-05 00:10:34 -0400

    Test to ensure the constructor checks to see if the container supports `\ArrayAccess`.

--- tests/PsrExceptionTest.php
+++ tests/PsrExceptionTest.php
@@ -1,1 +1,22 @@
+<?php declare(strict_types=1);
 
+namespace Omatamix\Container\Test;
+
+use Omatamix\Container\Container;
+use Omatamix\Container\Exception\ContainerException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the container builder.
+ */
+class BuilderTest extends TestCase
+{
+    /**
+     * @return void Returns nothing.
+     */
+    public function testString(): void
+    {
+        $this->expectException(ContainerException::class);
+        $builder = new Container(new Baz());
+    }
+}

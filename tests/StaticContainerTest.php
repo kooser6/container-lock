3cd2922e90e46eb9e9e6db64453e33b94c8d1e9b
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 20:14:19 -0400

    Create StaticContainerTest.php

---
+++ tests/StaticContainerTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Omatamix\Container\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the static container.
+ */
+class StaticContainerTest extends TestCase
+{
+    /**
+     * @return void Returns nothing.
+     */
+    public function testStaticContainer(): void
+    {
+        //
+    }
+}

1821db4ce0b78b472e642fbcf5dd9361a96ae39e
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 22:44:44 -0400

    Add the container builder code.

--- src/ContainerBuilder.php
+++ src/ContainerBuilder.php
@@ -30,4 +30,127 @@ namespace Omatamix\Container;
  */
 class ContainerBuilder implements BuilderInterface, LazyInterface, \ArrayAccess
 {
+    /** @var array $frozen */
+    private $raw = [];
+
+    /** @var array $values */
+    private $values = [];
+
+    /** @var array $frozen */
+    private $frozen = [];
+
+    /** @var array $protected */
+    private $protected = [];
+
+    /** @var \SplObjectStorage $services A container for our builder values. */
+    private $services;
+
+    /**
+     * Construct a new container builder.
+     *
+     * @param array $values A list of values to set.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(array $values = [])
+    {
+        $this->services = new \SplObjectStorage();
+        foreach ($values as $key => $value) {
+            if (!is_string($key)) {
+                throw new Exception\ContainerException('Error while setting values.');
+            }
+            $this->offsetSet($key, $value);
+        }
+    }
+
+    /**
+     * Checks if a parameter or an object is set.
+     *
+     * @param string $id The unique identifier for the parameter or object
+     *
+     * @return bool Returns true if the id exists and false if not.
+     */
+    public function offsetExists($id)
+    {
+        return array_key_exists($id, $this->values);
+    }
+
+    /**
+     * Put a new value in our values array.
+     *
+     * @param string $id Identifier of the entry we are putting.
+     *
+     * @return void Returns nothing.
+     */
+    public function offsetSet($id, $value)
+    {
+        if (isset($this->frozen[$id])) {
+            throw new Exception\ContainerException(sprintf('This `%s` identifier is frozen.', $id));
+        } elseif (is_array($value) && (!($value[0] instanceof \Closure) || !method_exists($value[0], '__invoke'))) {
+            $this->protected[$id] = $value[1];
+            $this->values[$id] = $value[0];
+        } else {
+            $this->values[$id] = $value;
+        }
+    }
+    
+    /**
+     * Retrieve a identifier's value.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return mixed Entry.
+     */
+    public function offsetGet($id)
+    {
+        if (!isset($this->values[$id])) {
+            throw new Exception\NotFoundException(sprintf('No entry was found for `%s` identifier.', $id));
+        } elseif (isset($this->raw[$id])
+            || !($callable instanceof \Closure)
+            || !method_exists($callable, '__invoke')
+            || $this->protected[$id]) {
+            return $this->values[$id];
+        } elseif (isset($this->services[$this->values[$id]])) {
+            return $this->values[$id]($this);
+        } else {
+            $raw = $this->values[$id];
+            $value = $this->values[$id] = $raw($this);
+            $this->raw[$id] = $raw;
+            $this->frozen[$id] = true;
+            return $value;
+        }
+    }
+
+    /**
+     * Unsets a parameter or an object.
+     *
+     * @param string $id The unique identifier for the parameter or object
+     *
+     * @return void Returns nothing.
+     */
+    public function offsetUnset($id)
+    {
+        if ($this->offsetExists($id)) {
+            if (isset($this->services[$this->values[$id]])) {
+                unset($this->services[$this->values[$id]]);
+            }
+            unset($this->values[$id], $this->frozen[$id], $this->raw[$id]);
+        }
+    }
+
+    /**
+     * Add new service definition.
+     *
+     * @param callable|\Closure $callable A closure callable or invokable object.
+     *
+     * @return array Returns the callable in an array.
+     */
+    public function service($callable, bool $protect = false)
+    {
+        if (!($callable instanceof \Closure) || !method_exists($callable, '__invoke')) {
+            throw new Exception\ContainerException('Callable is not a Closure or invokable object.');
+        }
+        $this->services->attach($callable);
+        return [$callable, $protect];
+    }
 }

d037721167a6fe3fe8a3d43eb997bc0442d20be5
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 23:12:35 -0400

    Add a test.

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -1,1 +1,23 @@
+<?php declare(strict_types=1);
 
+namespace Omatamix\Container\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the container builder.
+ */
+class BuilderTest extends TestCase
+{
+    /**
+     * @return void Returns nothing.
+     */
+    public function testString(): void
+    {
+        $builder = ContainerBuilder();
+        $builder['name'] = 'Nicholas';
+        $this->assertEquals($builder['name'], 'Nicholase');
+        unset($builder['name']);
+        $this->assertTrue(isset($builder['name']));
+    }
+}

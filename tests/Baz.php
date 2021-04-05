fccc705dfb7c9b18082f228e4c22ef75837f0685
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-05 00:08:49 -0400

    Add test class `baz`.

--- tests/Baz.php
+++ tests/Baz.php
@@ -1,1 +1,12 @@
+<?php declare(strict_types=1);
 
+namespace Omatamix\Container\Test;
+
+use Omatamix\Container\BuilderInterface;
+
+/**
+ * Test class.
+ */
+class Baz implements BuilderInterface
+{
+}

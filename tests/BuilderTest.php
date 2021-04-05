f0d804b317c0ff799e970a8bc8418430c3d7e7ca
omatamix <47761650+omatamix@users.noreply.github.com>
2021-04-04 23:49:40 -0400

    Add more test assertions for the psr-container.

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -2,6 +2,7 @@
 
 namespace Omatamix\Container\Test;
 
+use Omatamix\Container\Container;
 use Omatamix\Container\ContainerBuilder;
 use PHPUnit\Framework\TestCase;
 
@@ -21,5 +22,13 @@ class BuilderTest extends TestCase
         $this->assertTrue(isset($builder['name']));
         unset($builder['name']);
         $this->assertTrue(!isset($builder['name']));
+        $builder['name'] = 'Nicholas';
+        $builder['isDeveloper'] = 'Yes';
+        $psrContainer = new Container($builder);
+        $this->assertTrue($psrContainer->has('name'));
+        $this->assertTrue($psrContainer->has('isDeveloper'));
+        $this->assertEquals($psrContainer->get('name'), 'Nicholas');
+        $this->assertEquals($psrContainer->get('isDeveloper'), 'Yes');
+        $this->assertTrue(!$psrContainer->has('randomId'));
     }
 }
